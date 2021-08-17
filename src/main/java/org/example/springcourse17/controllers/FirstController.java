fe7d267a70b3c0c302099db8fc64e493f218ced3
pavel <ppolet@mail.ru>
2021-08-17 12:55:53 +0300

    Lesson 19 - Spring Framework. Урок 19 Параметры GET запроса. Аннотация @RequestParam

--- src/main/java/org/example/springcourse17/controllers/FirstController.java
+++ src/main/java/org/example/springcourse17/controllers/FirstController.java
@@ -3,18 +3,30 @@ package org.example.springcourse17.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first") //название может быть любым и URL, соответственно поменяется: http://localhost:8080/first/hello
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname){
+
+        System.out.println("GoodBye, " + name + " " + surname);
+
         return "first/goodbye";
     }
 }
